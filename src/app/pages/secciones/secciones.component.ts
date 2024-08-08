4ffae194e6e6f58d46a9120815fe6a8e6dd1b11a
ludmilaramonda <145378638+ludmilaramonda@users.noreply.github.com>
2024-08-08 09:28:15 -0300

    componente seccion

---
+++ src/app/pages/secciones/secciones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-secciones',
+  standalone: true,
+  imports: [],
+  templateUrl: './secciones.component.html',
+  styleUrl: './secciones.component.css'
+})
+export class SeccionesComponent {
+
+}
