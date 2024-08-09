9288e6b79bb0a6ba6149244dc547fff8bcf49915
Giovanni Peirone <mgpeirone@escuelasproa.edu.ar>
2024-08-09 12:51:39 -0300

    Rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MarcadorDePuntosComponent } from './pages/marcador-de-puntos/marcador-de-puntos.component';
+import { ExplicacionRutasComponent } from './explicacion-rutas/explicacion-rutas.component';
+import { InformativaComponent } from './pages/informativa/informativa.component';
+import { CalendarioComponent } from './pages/calendario/calendario.component';
+import { SeccionesComponent } from './pages/secciones/secciones.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ExplicacionRutasComponent,//borrar despues
+    //HeaderComponent,
+    //FooterComponent , 
+    MarcadorDePuntosComponent,
+    InformativaComponent,
+    SeccionesComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/explicacion-rutas/explicacion-rutas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-explicacion-rutas',
+  standalone: true,
+  imports: [],
+  templateUrl: './explicacion-rutas.component.html',
+  styleUrl: './explicacion-rutas.component.css'
+})
+export class ExplicacionRutasComponent {
+
+}
