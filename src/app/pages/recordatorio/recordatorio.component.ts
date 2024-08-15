e204f0f6f304ae46ad4780c5303843ee8801f14e
Zoeblanco <115807515+Zoeblanco@users.noreply.github.com>
2024-08-15 09:13:19 -0300

    remember
    Hola

---
+++ src/app/pages/recordatorio/recordatorio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recordatorio',
+  standalone: true,
+  imports: [],
+  templateUrl: './recordatorio.component.html',
+  styleUrl: './recordatorio.component.css'
+})
+export class RecordatorioComponent {
+
+}
