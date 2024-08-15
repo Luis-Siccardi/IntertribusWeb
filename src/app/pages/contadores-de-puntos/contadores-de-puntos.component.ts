b48972d166911a7ea9a231cee0254458ef1bba51
Giovanni <134148500+GiovanniPeirone@users.noreply.github.com>
2024-08-15 09:00:14 -0300

    z

---
+++ src/app/pages/contadores-de-puntos/contadores-de-puntos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contadores-de-puntos',
+  standalone: true,
+  imports: [],
+  templateUrl: './contadores-de-puntos.component.html',
+  styleUrl: './contadores-de-puntos.component.css'
+})
+export class ContadoresDePuntosComponent {
+
+}
