efae4331c981bc6df6cdcf581ffb7417ffd1281f
maxi <marquezmaxi777@gmail.com>
2024-08-21 08:43:44 -0300

    b
    h

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,13 @@ import { InformativaComponent } from './pages/informativa/informativa.component'
 import { SeccionesComponent } from './pages/secciones/secciones.component';
 import { ContadoresDePuntosComponent } from './pages/contadores-de-puntos/contadores-de-puntos.component';
 import { FooterComponent } from './shared/footer/footer.component';
+<<<<<<< HEAD
 import { RecordatorioComponent } from './pages/recordatorio/recordatorio.component';
 
+=======
+import { LoginComponent } from './shared/login/login.component';
+import { RegisterComponent } from './shared/register/register.component';
+>>>>>>> 79960a84928370a39240ec260f546778d0f18e50
 
 
 export const routes: Routes = [
@@ -15,8 +20,14 @@ export const routes: Routes = [
     {path: 'informativa', component:InformativaComponent},
     {path: 'secciones', component:SeccionesComponent},
     {path: 'contadoresDePuntos', component:ContadoresDePuntosComponent},
+<<<<<<< HEAD
     {path: 'footer', component:FooterComponent}
     {path: 'recordatorio', component:RecordatorioComponent}
     
+=======
+    {path: 'footer', component:FooterComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent}
+>>>>>>> 79960a84928370a39240ec260f546778d0f18e50
     //{path: 'header', component:HeaderComponent}
 ];
